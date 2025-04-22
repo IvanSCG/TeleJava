bb8c3bccdc6bced23310eb4728d44ab35935fe44
ivansc2-admin <ivansanchezcuerva@gmail.com>
2025-04-22 17:25:27 +0200

    Subida inicial

---
+++ Server.js
@@ -0,0 +1,21 @@
+// server.js
+const net = require('net');
+const clients = [];
+
+const server = net.createServer(socket => {
+  clients.push(socket);
+
+  socket.on('data', data => {
+    // Broadcast a todos los demás
+    clients.forEach(c => {
+      if (c !== socket) c.write(data);
+    });
+  });
+
+  socket.on('end', () => {
+    const i = clients.indexOf(socket);
+    if (i !== -1) clients.splice(i, 1);
+  });
+});
+
+server.listen(process.env.PORT || 12345);
