ecec50f479bf57abef3596c637aef44c356f3706
ivansc2-admin <ivansanchezcuerva@gmail.com>
2025-05-21 14:21:06 +0200

    Control de avatares

--- server.js
+++ server.js
@@ -6,8 +6,17 @@ const PORT = process.env.PORT || 12345; // Railway asignará PORT automáticamen
 net.createServer(socket => {
   clients.push(socket);
   socket.on('data', data => {
+      try {
+      const msg = JSON.parse(data);
+      if (msg.type && msg.type === 'register') {
+        // no reenviamos los registros
+        return;
+      }
+    } catch (_) {
+      // no es JSON, seguimos adelante
+    }
     // retransmite a todos menos al emisor
-    clients.forEach(c => { if (c !== socket) c.write(data); });
+    clients.forEach(c => { if (c !== socket) c.write(data); });  
   });
   socket.on('end', () => {
     const i = clients.indexOf(socket);
